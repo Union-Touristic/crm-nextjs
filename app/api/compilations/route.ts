b6f4adce6f41037aaae8862533f2a1c7e36a889f
Alibek Massalimov <alibekmslv@gmail.com>
2023-10-15 16:03:04 +0300

    Route Handler for tours compilations

---
+++ app/api/compilations/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { TourInsert, compilations, tours, users } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { getToken } from "next-auth/jwt";
+
+export async function GET(request: NextRequest) {}
+
+export async function POST(request: NextRequest) {
+  const token = await getToken({ req: request });
+
+  try {
+    // User has token
+    if (token?.email) {
+      const email = token.email;
+
+      const [user] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email))
+        .limit(1);
+
+      const data: TourInsert[] = await request.json();
+
+      const [createdCompilation] = await db
+        .insert(compilations)
+        .values({
+          userId: user.id,
+        })
+        .returning();
+
+      const toursToInserst = data.map((tour) => {
+        return {
+          ...tour,
+          compilationId: createdCompilation.id,
+        };
+      });
+
+      await db.insert(tours).values(toursToInserst);
+    }
+
+    return new NextResponse(null, { status: 201 });
+  } catch (error) {
+    console.log(error);
+  }
+}
